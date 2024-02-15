d8b89cb9af663ab382b05e69a7f092688f7e829b
Kenny <kennyvungo@gmail.com>
2024-02-15 03:53:45 -0800

    fixed portfolio link, added active tabs

--- components/header.tsx
+++ components/header.tsx
@@ -1,31 +1,38 @@
-import React from 'react'
-import Image from 'next/image'
-import logo from '../public/logo.png'
-import Link from 'next/link'
+"use client";
+import React from "react";
+import Image from "next/image";
+import logo from "../public/logo.png";
+import Link from "next/link";
 import { CiInstagram } from "react-icons/ci";
 import { FaInstagram } from "react-icons/fa6";
+import { usePathname } from "next/navigation";
 
-export default function Header(){
+export default function Header() {
+  const pathname = usePathname();
   return (
     <div className="flex flex-row items-center bg-white h-24 text-black font-sans w-screen bg-opacity-75">
       <div className="flex flex-row  w-1/5 justify-around">
-        <div >
+        <div className={pathname == "/" ? "underline" : ""}>
           <Link href="/">HOME</Link>
         </div>
-        <div>
-          <Link href="/works">PORTFOLIO</Link>
+        <div className={pathname == "/portfolio" ? "underline" : ""}>
+          <Link href="/portfolio">PORTFOLIO</Link>
         </div>
       </div>
-      <div className='w-3/5 flex flex-row justify-center '>
+      <div className="w-3/5 flex flex-row justify-center ">
         <Link href="/">
-          <Image src={logo} alt="logo" width="64" height="64"/>
+          <Image src={logo} alt="logo" width="64" height="64" />
         </Link>
       </div>
-      <div className='flex flex-row w-1/5'>
-        <span className='w-2/4'> </span>
-        <Link href="https://www.instagram.com/by_unumcornu/" className='flex flex-row justify-center text-2xl w-2/4'><FaInstagram/></Link>
+      <div className="flex flex-row w-1/5">
+        <span className="w-2/4"> </span>
+        <Link
+          href="https://www.instagram.com/by_unumcornu/"
+          className="flex flex-row justify-center text-2xl w-2/4"
+        >
+          <FaInstagram />
+        </Link>
       </div>
     </div>
   );
 }
-
