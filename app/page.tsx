29b49047fe5908b47b5c5b26f1ecbd2d445b631f
Kenny <kennyvungo@gmail.com>
2024-02-15 03:03:38 -0800

    added additional panels

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,20 @@
 import Image from "next/image";
 import Header from "../components/header";
 import Card from "@/components/card";
-import panel from '../public/panel1.png'
+import panel1 from '../public/panel1.png'
+import panel2 from '../public/panel2.png'
+import panel3 from '../public/panel3.png'
+import panel4 from '../public/panel4.png'
+import panel5 from '../public/panel5.png'
 export default function Home() {
   return (
     <main className="text-black overflow-y-auto h-screen w-screen flex flex-col items-center pb-28">
       <div className= "w-2/4">
-      <Card image={panel}/>
-      <Card image={panel}/>
-      <Card image={panel}/>
-      <Card image={panel}/>
-      <Card image={panel}/>
-      <Card image={panel}/>
-      <Card image={panel}/>
-      <Card image={panel}/>
-    
+      <Card image={panel1}/>
+      <Card image={panel2}/>
+      <Card image={panel3}/>
+      <Card image={panel4}/>
+      <Card image={panel5}/>
       </div>
     </main>
   );
