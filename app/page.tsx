0821a30a615b5d0a45242ea4082dd2f47b6d05ac
Kenny <kennyvungo@gmail.com>
2024-02-15 19:53:52 -0800

    fixed panel path

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,5 @@
-import Image from "next/image";
-import Header from "../components/header";
+
 import Card from "@/components/card";
-import panel1 from '../public/panel1.png'
-import panel2 from '../public/panel2.png'
-import panel3 from '../public/panel3.png'
-import panel4 from '../public/panel4.png'
-import panel5 from '../public/panel5.png'
 export default function Home() {
   let panels = [1,2,3,4,5]
   return (
