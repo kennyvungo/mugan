0a9faa9c3057ec0418664616884da1d36538fca3
Kenny <kennyvungo@gmail.com>
2024-02-15 20:18:30 -0800

    hover zoom effect for gallery

--- components/product.tsx
+++ components/product.tsx
@@ -5,12 +5,19 @@ import Image from 'next/image'
 import cup from '../public/1.jpg';
 const Product = ({prodNum}:{prodNum:any}) => {
   return (
-    <figure className='my-auto'>
-        <span className="relative block transition duration-500 ease-in-out transform rounded">
-            <Image width={300} height={300} className="relative block object-contain object-center w-full h-full" loading="lazy" alt="logo" src={`/gallery/${prodNum}.jpg`}></Image>
-        </span>
+    <figure className="my-auto overflow-hidden">
+      <span className="relative block transition duration-300 transform origin-center hover:scale-105">
+        <Image
+          width={300}
+          height={300}
+          className="relative block object-contain object-center w-full h-full"
+          loading="lazy"
+          alt="logo"
+          src={`/gallery/${prodNum}.jpg`}
+        ></Image>
+      </span>
     </figure>
-  )
+  );
 }
 
 export default Product
