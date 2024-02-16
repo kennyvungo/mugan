5dbb2edf8c0cd182d1a1c4e2a7894eb78f112ccb
Kenny <kennyvungo@gmail.com>
2024-02-15 23:29:02 -0800

    disable pointer for landing

--- components/card.tsx
+++ components/card.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 const Card = ({imageNum}:{imageNum:any}) => {
   return (
     <>
-      <div className="sm:mb-mobile md:mb-vh-bottom">
+      <div className="sm:mb-mobile md:mb-vh-bottom pointer-events-none">
         <Image
           src={`/panels/panel${imageNum}.png`}
           alt="panel"
