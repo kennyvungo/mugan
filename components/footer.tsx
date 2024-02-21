7309c031c79d8e6ee0598b3b1c51e0513a6fc28a
Kenny <kennyvungo@gmail.com>
2024-02-20 22:42:22 -0800

    fixed spacing on footer for mobile

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className="flex flex-row w-full items-center">
-        <div className="flex flex-row justify-end w-1/5 text-xs ">unumcornu 2024</div>
+        <div className="flex flex-row justify-end w-1/5 text-xs sm:ml-mobile ">unumcornu 2024</div>
       <span className="w-3/5"></span>
       <a
         href="https://www.instagram.com/by_unumcornu"
