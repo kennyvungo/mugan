06d9d3627b36fde67f56a2356ed8a2988e9c516c
Kenny <kennyvungo@gmail.com>
2024-03-18 22:39:47 -0700

    added cypress data tags

--- components/email.tsx
+++ components/email.tsx
@@ -20,7 +20,7 @@ const [showForm,setshowForm] = useState(true)
   return (
     <>
     {showForm ?
-        <form className="w-1/2 sm:w-4/5" onSubmit={handleSubmit(onSubmit)}>
+        <form className="w-1/2 sm:w-4/5" data-cy="email-form" onSubmit={handleSubmit(onSubmit)}>
         <div className="mb-5">
         <label
         className="mb-3 block text-base font-medium text-black"
@@ -31,6 +31,7 @@ const [showForm,setshowForm] = useState(true)
         type="text"
         placeholder="Full Name"
         className="w-full rounded-md border border-gray-300 bg-white py-3 px-6 text-base font-medium text-gray-700 outline-none focus:border-red-700 focus:shadow-md"
+        data-cy="name-input"
         {...register("name", { required: true })}
         />
         </div>
@@ -43,6 +44,7 @@ const [showForm,setshowForm] = useState(true)
         <input
         type="email"
         placeholder="example@gmail.com"
+        data-cy="email-input"
         className="w-full rounded-md border border-gray-300 bg-white py-3 px-6 text-base font-medium text-gray-700 outline-none focus:border-red-700 focus:shadow-md"
         {...register("email", { required: true })}
         />
@@ -56,18 +58,19 @@ const [showForm,setshowForm] = useState(true)
         <textarea
         rows={4}
         placeholder="Type your message"
+        data-cy="message-input"
         className="w-full resize-none rounded-md border border-gray-300 bg-white py-3 px-6 text-base font-medium text-gray-700 outline-none focus:border-red-700 focus:shadow-md"
         {...register("message", { required: true })}
         ></textarea>
         </div>
         <div>
-        <button className="hover:bg-gray-600 rounded-lg py-1 pt-2 px-4 text-white bg-[#3B3B3B] font-semibold outline-none">
+        <button className="hover:bg-gray-600 rounded-lg py-1 pt-2 px-4 text-white bg-[#3B3B3B] font-semibold outline-none" data-cy="submit-button">
             SUBMIT
         </button>
         </div>
         </form>
         : <div className='h-3/5 justify-around text-l flex flex-col items-center font-sans'>
-            <div className='justify-center flex flex-col items-center'>
+            <div className='justify-center flex flex-col items-center' data-cy="thanks-alert">
                 <div className='mb-4'>
                 THANK YOU! 
                 </div>
